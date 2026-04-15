82dfb628ab54046d41cbdbb9b4b15f9ae43f8383
Shivanshu Srivastava <151041980+shivanshusri1102@users.noreply.github.com>
2026-04-15 08:26:33 +0530

    Time: 305 ms (46.97%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,8 @@
+SELECT id, COUNT(*) AS num
+FROM(
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted) AS t
+    GROUP BY id
+    ORDER BY num DESC
+    LIMIT 1;
