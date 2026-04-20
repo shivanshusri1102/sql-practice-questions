61ed9bc86c4dcda24e864c6fc879a67e5e3e830d
Shivanshu Srivastava <151041980+shivanshusri1102@users.noreply.github.com>
2026-04-20 13:24:36 +0530

    Time: 773 ms (33.79%), Space: 0B (100%) - LeetHub

---
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.sql
@@ -0,0 +1,16 @@
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';
+
+
+/* SELECT user_id, name, mail
+FROM Users
+WHERE mail LIKE '%@leetcode.com'
+AND mail NOT LIKE '.%@leetcode.com'
+AND mail NOT LIKE '%#%' */
